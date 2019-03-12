3c3d3890efa2a743cb43fd3848040279d0f53851
Jacob Larsen <powerturtle@protonmail.ch>
2019-03-12 01:28:45 +0100

    Add project files.

---
+++ RestCustomerService/Controllers/CustomerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RestCustomerService.Model;
+
+namespace RestCustomerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private static List<Customer> cList = new List<Customer>();
+
+        // GET: api/Customer
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Customer/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Customer
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Customer/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RestCustomerService/Model/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace RestCustomerService.Model
+{
+    public class Customer
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private DateTime _year;
+
+        public Customer(int id, string fristname, string lastname, DateTime year)
+        {
+            _id = id;
+            _firstName = fristname;
+            _lastName = lastname;
+            _year = year;
+        }
+        public Customer() { }
+
+        public int ID
+        {
+            get { return _id; }
+            set { value = _id; }
+            
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { value = _firstName; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { value = _lastName; }
+        }
+
+        public DateTime Year
+        {
+            get { return _year; }
+            set { value = _year; }
+        }
+    }
+}
